aae0c7a6136eb543497ebc800a7d7e7494ecd6a1
kevinrizkhy <kevin.rizkhy85@gmail.com>
2019-05-15 02:28:21 +0700

    [user, items] index

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,6 +4,7 @@ class HomesController < ApplicationController
     @total_limit_items = StoreItem.where(store_id: current_user.store.id).where('stock < min_stock').count
     @total_orders = Order.where(store_id: current_user.store.id).where('date_receive is null').count
     @total_payments = Order.where(store_id: current_user.store.id).where('date_receive is not null and date_paid_off is null').count
+    @total_returs = Retur.where(store_id: current_user.store.id).where('date_picked is not null').count
   	# UserMailer.welcome_email("kevin.rizkhy85@gmail.com", "Subject 1").deliver
   end
 
