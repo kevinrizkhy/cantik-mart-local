1d897614afacc6abd00739e19157bbe9f39aa94b
kevinrizkhy <kevin.rizkhy85@gmail.com>
2019-06-04 20:46:25 +0700

    filter in activities was working normally, but in index, show selected item

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,25 +1,27 @@
 class ActivitiesController < ApplicationController
   
 	def index
-		
-		@models = Controller.all.pluck(:name)
+		@models = PublicActivity::Activity.pluck(:trackable_type)
 	  	@activities = PublicActivity::Activity.order("created_at desc").page param_page
 		
-		@date_search = params[:activity][:date_search]
-		@access_levels = params[:activity][:access_levels]
-		@targets = params[:activity][:targets]
-		
+		@date_search = DateTime.now.to_date
+		if params[:activity].present?
+			@date_search = params[:activity][:date_search] 
+			@access_levels = params[:activity][:access_levels] 
+			@targets = params[:activity][:targets]
+		end
 		if @date_search.present?
 			@activities = @activities.where(created_at: @date_search)
 		end
 
 		if @access_levels.present?
-			
+			users = User.where(level: @access_levels).pluck(:id)
+			@activities = @activities.where(owner_id: users)
 		end
 
 		if @targets.present?
+			@activities = @activities.where(trackable_type: @targets)
 		end
-		binding.pry
 		# @activities.delete_all
 	end
 
