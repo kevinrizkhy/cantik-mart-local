f81041ac34a31221819007e85c3eb6c5aafb893a
kevinrizkhy <kevin.rizkhy85@gmail.com>
2019-08-19 15:11:34 +0700

    due_date

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -19,19 +19,26 @@ class DebtsController < ApplicationController
       search_text = "Pencarian "
       filters = Debt.page param_page
 
+      due_date = params[:due_date]
+      if due_date == 1
+        filters = filters.where("due_date <= ?", Date.today.end_of_week)
+      end
+
       switch_data_month_param = params[:switch_date_month]
       if switch_data_month_param == "month" 
         before_months = params[:months].to_i
         search_text += before_months.to_s + " bulan terakhir "
         start_months = (DateTime.now - before_months.months).beginning_of_month 
         filters = filters.where("date_created >= ?", start_months)
-      else
+      elsif switch_data_month_param == "date"
         end_date = DateTime.now.to_date + 1.day
         start_date = DateTime.now.to_date - 1.weeks
         end_date = params[:end_date] if params[:end_date].present?
         start_date = params[:date_from] if params[:date_from].present?
         search_text += "dari " + start_date.to_s + " hingga " + end_date.to_s + " "
         filters = filters.where("date_created >= ? AND date_created <= ?", start_date, end_date)
+      else
+        
       end
 
       if params[:order_by] == "asc"
