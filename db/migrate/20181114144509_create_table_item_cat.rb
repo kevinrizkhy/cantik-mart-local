fbfc82995f03a9330e2ef6281a8ca86e1104bde9
kevinrizkhy <kevin.rizkhy85@gmail.com>
2019-09-07 14:09:27 +0700

    timestamps

--- db/migrate/20181114144509_create_table_item_cat.rb
+++ db/migrate/20181114144509_create_table_item_cat.rb
@@ -2,6 +2,8 @@ class CreateTableItemCat < ActiveRecord::Migration[5.2]
   def change
     create_table :item_cats do |t|
       t.string :name, null: false, default: 'DEFAULT'
+
+      t.timestamps
     end
   end
 end
