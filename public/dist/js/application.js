f12354170e301eaedb430427d8bf465a20006247
kevinrizkhy <kevin.rizkhy85@gmail.com>
2022-05-16 14:01:39 +0700

    api

--- public/dist/js/application.js
+++ public/dist/js/application.js
@@ -94,16 +94,23 @@ function getTotalCurrDate(input_date){
 
 
   setTimeout(function (){
-    if( (cloud_data.length != local_data.length) || (local_data.length == 0)) {
+    if(local_data.length == 0) {
       $("#notice_sync").html("TIDAK DAPAT MELAKUKAN SYNC");
       $("#notice_sync").show();
       $("#notice_not_sync").hide();
       $("#daily_sync_button").hide();
     }else{
-      if((cloud_data[0]==local_data[0]) && (cloud_data[1]==local_data[1]) ){
-        $("#notice_not_sync").show();
-        $("#notice_sync").hide();
-        $("#daily_sync_button").hide();
+      if(cloud_data[0]==local_data[0]){
+        if (cloud_data[1]==local_data[1]){
+          $("#notice_not_sync").show();
+          $("#notice_sync").hide();
+          $("#daily_sync_button").hide();
+        }else{
+          $("#notice_sync").html("TERDAPAT RETUR KONSUMEN, SILAHKAN CEK SECARA MANUAL");
+          $("#notice_not_sync").hide();
+          $("#notice_sync").show();
+          $("#daily_sync_button").hide();
+        }
       }else{
         if (cloud_data[0] > local_data[0]){
           $("#notice_sync").html("TERDAPAT RETUR KONSUMEN, SILAHKAN CEK SECARA MANUAL");
